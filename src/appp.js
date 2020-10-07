31099f8590747994df4935e17808a0a60e4865b4
rpnX <shyiskI@gmail.com>
2020-10-07 21:51:07 +0300

    add+ dell todos
    add todos + del todos func

--- src/appp.js
+++ src/appp.js
@@ -7,7 +7,7 @@ class Appp extends Component {
     constructor() {
         super();
         this.state ={
-            
+            newTodo: "",
             todos:[
                 { id: 1, text:"1 todo" },
                 { id: 2, text:"2 todo" },
@@ -16,24 +16,79 @@ class Appp extends Component {
                 { id: 5, text:"5 todo" },
             ]
         }
+
+        this.handleChange = this.handleChange.bind(this)
+        this.addTodo = this.addTodo.bind(this)
+        this.generatorId = this.generatorId.bind(this)
+        this.deleteTodo = this.deleteTodo.bind(this)
+    }
+
+    handleChange(event) {
+
+        this.setState({
+            newTodo: event.target.value
+        })
+
+    }
+    generatorId() {
+        const lastId = this.state.todos[this.state.todos.length - 1]
+        if (lastId){
+            return lastId.id + 1 
+        }
+        return 1
+    }
+    addTodo() {
+        const newTodo = {
+            id: this.generatorId(),
+            text: this.state.newTodo   
+        }
+        const todos = this.state.todos
+        todos.push(newTodo)
+
+        this.setState({
+            todos: todos, 
+            newTodo: ""
+        })
+    }
+
+    deleteTodo(key) {
+
+        const todos = this.state.todos
+        delete todos[key]
+
+        this.setState({
+            todos
+        })
+
     }
 
     render() {
+
         return (
         <div className="App">
             <header className="App-header">
                 TODO
             </header>
+
+            <input
+            type="text"
+            name="todo"
+            className="formcotrol"
+            placeholder="add new todo"
+            onChange={this.handleChange}
+            value={this.state.newTodo}
+            />
+            <button className="addtodo"
+            onClick={this.addTodo}
+            >add todo</button>
+
             <div className="list">
                 {this.state.todos.map((item, key)=>(
-                
                     <div key={key} className="item" id={item.id}>
                         {item.text}
-                        <button className="update">u</button>
-                        <button className="dell">X</button>
+                        <button className="update">U</button>
+                        <button className="dell" onClick={() => {this.deleteTodo(key)}}>X</button>
                     </div>
-                
-                
                     )
                 )}
             </div>
